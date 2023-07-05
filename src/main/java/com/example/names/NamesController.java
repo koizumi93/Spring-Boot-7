c8293a474d7bb529def8e4c4c09ad446f244c97d
koizumi93 <limeizhenguo5@gmail.com>
2023-07-05 16:14:03 +0900

    各メソッドの記述の追加と変更

--- src/main/java/com/example/names/NamesController.java
+++ src/main/java/com/example/names/NamesController.java
@@ -1,11 +1,13 @@
 package com.example.names;
 
+import jakarta.validation.Valid;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
+import java.time.LocalDate;
 import java.util.Map;
 
 
@@ -13,21 +15,33 @@ import java.util.Map;
 public class NamesController {
     
     @GetMapping("/names")
-    public List<String> getNames(){
-        return List.of("yamada","hanako");
+    public String getNames(@RequestParam("name")String name,
+                           @RequestParam("dateOfBirth")
+                           @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate dateOfBirth){
+        return "getNames-page";
     }
     
     @PostMapping("/names")
-    public ResponseEntity<String> create(@RequestBody CreateForm form){
+    public ResponseEntity<Map<String,String>> create(@Valid @RequestBody CreateForm form){
         URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
                 .path("/names/id")
                 .build()
                 .toUri();
-        return ResponseEntity.created(url).body("name successfully created");
+        /*if (form.getName()==null || form.getName().isEmpty()){
+            return ResponseEntity.badRequest().body("Name is required");
+        }
+        if (form.getName().length()>20){
+            return ResponseEntity.badRequest().body("名前を20文字以下で入力してください");
+        }*/
+        return ResponseEntity.created(url).body(Map.of("message","name successfully created"));
     }
     @PatchMapping("/names/{id}")
     public ResponseEntity<Map<String,String>> update(@PathVariable("id")int id,@RequestBody UpdateForm form){
         //更新処理は省略
         return ResponseEntity.ok(Map.of("message","name successfully updated"));
+    }
+    @DeleteMapping("/names/{id}")
+    public ResponseEntity<Map<String,String>> deleteName(@PathVariable("id")int id){
+        return ResponseEntity.ok(Map.of("message","name successfully deleted"));
     }
 }
--- src/main/java/com/example/names/UpdateForm.java
+++ src/main/java/com/example/names/UpdateForm.java
@@ -6,5 +6,5 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UpdateForm {
-    public String name;
+    private String name;
 }
